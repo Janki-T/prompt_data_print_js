7965e571f67c7537023ffcab0cc21a0693600d4a
Janki-T <jankithanki1304@gmail.com>
2022-09-28 17:26:40 +0530

    first commit

---
+++ js/main1.js
@@ -0,0 +1,4 @@
+alert("Welcome");
+let User_Name = prompt("Plz Enter Your Name");
+//alert("Welcome to my page" + " " + User_Name);
+let user_ans = document.getElementById("demo").innerHTML = "Welcome to my page" + " " + User_Name
